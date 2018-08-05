a9948dac9181197e335a4d20c52cda51723ca9dc
somnathX <somnathrs25@gmail.com>
2018-08-05 16:34:07 +0530

    Move zeroes problem added

---
+++ rathbone/283. Move Zeroes.cpp
@@ -0,0 +1,28 @@
+283. Move Zeroes
+/***
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+***/
+
+/***
+Approach:
+Iterate from 0 to n-1
+Keep track of last non zero index.
+Swap last non zero element with current if current element is non zero
+***/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.size(); cur++) {
+            if (nums[cur] != 0) {
+                swap(nums[lastNonZeroFoundAt++], nums[cur]);
+            }
+        }
+    }
+};
